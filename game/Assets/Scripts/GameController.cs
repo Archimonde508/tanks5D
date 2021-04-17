dbe2e358b2f7767dc55ba5a9d58ef2d35e16767f
Archimonde508 <61909525+Archimonde508@users.noreply.github.com>
2021-04-17 11:32:23 +0200

    Connection class created

--- game/Assets/Scripts/GameController.cs
+++ game/Assets/Scripts/GameController.cs
@@ -14,6 +14,7 @@ public class GameController : MonoBehaviour
 
     private void Awake()
     {
+        Connection.func();
         // z -34 do 18
         spawnPositionsZ.Add(-34f);
         spawnPositionsZ.Add(18f);
